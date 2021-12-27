f4b97f0973d3411688a53cbbfc5cb048d6b8fdb4
ouyangpeng <oypcz@foxmail.com>
2021-12-27 18:55:32 +0800

    1. 修复 移植 转场效果 方向正好反了的bug

--- app/src/main/assets/fragment/fragment_shader_transition20.glsl
+++ app/src/main/assets/fragment/fragment_shader_transition20.glsl
@@ -11,9 +11,12 @@ uniform float u_offset;
 uniform vec2 u_texSize;
 
 
-float inHeart (vec2 p, vec2 center, float size) {
+float inHeart (vec2 uv, vec2 center, float size) {
     if (size==0.0) return 0.0;
-    vec2 o = (p - center)/(1.6 * size);
+
+    // 这里 得把坐标改一下，否则心形是反的  uv 改成 -uv
+    //    vec2 o = (uv + center)/(1.6 * size);
+    vec2 o = (-uv + center)/(1.6 * size);
     float a = o.x * o.x + o.y * o.y - 0.3;
     // step(edge,x)  如果x < edge，返回0.0，否则返回1.0
     return step(a * a * a, o.x * o.x * o.y * o.y * o.y);
@@ -23,7 +26,7 @@ vec4 transition (vec2 uv) {
     return mix(
         texture(u_texture0,uv),
         texture(u_texture1,uv),
-        inHeart(uv, vec2(0.5, 0.4), u_offset)
+        inHeart(uv, vec2(0.5, 0.5), u_offset)
     );
 }
 
